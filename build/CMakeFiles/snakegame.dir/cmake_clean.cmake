f9125e61da5095ac069a4ee50ffbd97b2fd347a4
Juma Fernández <73233980+jumiita@users.noreply.github.com>
2024-01-04 18:38:18 +0100

    aqui funciona el hola mundo

---
+++ build/CMakeFiles/snakegame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snakegame.dir/main.cpp.o"
+  "CMakeFiles/snakegame.dir/main.cpp.o.d"
+  "snakegame"
+  "snakegame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snakegame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
